67aafaf90411d596b62677ed421cc45338465e8d
lltchen <lltchen1@gmail.com>
2019-02-12 20:59:59 -0500

    Delete]d User Stories Folder

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 require 'bundler/setup'
-require_relative '../lib/user_stories/introduction.rb'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
